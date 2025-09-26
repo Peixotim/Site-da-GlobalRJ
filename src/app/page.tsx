cfa08bf373eedba69b6933c64c98ab2cb97a5ac6
Peixotim <pedropeixotovz@gmail.com>
2025-09-26 10:07:54 -0300

    fix:Refatora dados da pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@ import CertifiersShowcaseV3 from "@/components/Certifiers";
 import FooterPremium from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import SaibaMais from "@/components/SaibaMais";
-import StatsBarNeo from "@/components/Stats";
 import EmpresasShowcase from "@/components/EmpresasShowCase";
+import Stats from "@/components/Stats";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <SaibaMais />
-      <StatsBarNeo className="mt-32" />
+      <Stats className="mt-20" />
       <CertifiersShowcaseV3 className="mt-28" />
       <EmpresasShowcase className="mt-32" />
 
